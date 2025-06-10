136642db159c9026571299605e7c996a0f659dd6
Vaishakh <vaishakhsubramonain@gmail.com>
2025-06-10 10:49:16 +0000

    added the delete api, for parent task

--- app/api/tasks/[taskID]/route.ts
+++ app/api/tasks/[taskID]/route.ts
@@ -1,15 +1,16 @@
 import { NextResponse, NextRequest } from "next/server";
 import { supabase } from "@/lib/supabase";
 import { auth } from "@clerk/nextjs/server";
-import { parentTask } from "@/types/task";
+import { isChildTask, parentTask } from "@/types/task";
 
 async function getUserID () {
     const { userId } = await auth();
     return userId
 }
 
-export async function GET(taskID: string) {
+export async function GET( request: NextRequest, { params }: { params: { taskID: string } }) {
     const userID = await getUserID();
+    const { taskID } = params;
 
     const { data, error } = await supabase
         .from('parent_tasks')
@@ -20,9 +21,9 @@ export async function GET(taskID: string) {
     if(error){
         return NextResponse.json({error : error.message })
     }
-    const task : parentTask[] = data || []
+    
+    const task: parentTask = data[0] || null
     return NextResponse.json(task);
-
 }
 
 export async function PUT (request: NextRequest) {
@@ -41,4 +42,22 @@ export async function PUT (request: NextRequest) {
         }
 
         return NextResponse.json({message: "Parent Task successfully created"});
+}
+
+export async function DELETE(request: NextRequest){
+    const data = await request.json();
+    if(isChildTask(data)) {
+        const { error } = await supabase
+            .from('child_tasks')
+            .delete()
+            .eq('id',data.id)
+            .eq('parentTask',data.parentTask)
+    
+        if(error) {
+            console.log(error.message)
+            return NextResponse.json(error.message);
+        }
+        return NextResponse.redirect(new URL(`/tasks${data.parentTask}`,request.url))
+    }
+    
 }
