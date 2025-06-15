e1feeb695fe44fba03cf5a1ed7117857b5cb7199
Your Name <vaishakhsubramonian@gmail.com>
2025-06-15 23:31:45 +0530

    added the update task fucntionality

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -23,7 +23,7 @@ const Data = () => {
 const TasksLayout: React.FC = () => {
 
     const tasks = Data()
-
+    console.log(tasks);
     return(
         <section className="min-h-screen bg-gray-50 py-8 px-4">
             <div className="max-w-7xl mx-auto">
