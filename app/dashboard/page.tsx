d4cc0c141c6b8af36291bdd856c0de37b75aec21
C S Vaishakh <vaishakhsubramonian@.com>
2025-06-18 17:51:46 +0530

    added the header to the structure of the dashboard page

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { HomeHeader } from "@/components/header";
+import React from "react";
+
+const Dashborad: React.FC = () => {
+    return(
+        <section>
+            <section>
+                <HomeHeader/>
+            </section>
+            <section>
+                <h1>Comming soon</h1>
+            </section>
+        </section>
+    );
+};
