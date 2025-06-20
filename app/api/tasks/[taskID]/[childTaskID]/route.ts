c344d244e1bddb5d2d6d01376b3b0357a7e7dd0c
Your Name <vaishakhsubramonian@gmail.com>
2025-06-20 22:15:32 +0530

    implemmnted the updation logic for parent_tasks table when a child task is deleted

--- app/api/tasks/[taskID]/[childTaskID]/route.ts
+++ app/api/tasks/[taskID]/[childTaskID]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { supabase } from "@/lib/supabase";
-import { childTask } from "@/types/task";
+import { childTask, parentTask } from "@/types/task";
 import { auth } from "@clerk/nextjs/server";
 
 
@@ -28,6 +28,7 @@ export async function GET(request: NextRequest, { params }: {params: {taskID: st
 }
 
 export async function DELETE(request: NextRequest){
+    const userID = await getUserID();
     const data = await request.json();
     const { error } = await supabase
         .from('child_tasks')
@@ -39,5 +40,29 @@ export async function DELETE(request: NextRequest){
         console.log(error.message);
         return NextResponse.json(error.message);
     }
+
+    const { data: parentTask } = await supabase
+                                        .from('parent_tasks')
+                                        .select('*')
+                                        .eq('id', data.parentTask)
+                                        .eq('user_id',userID)
+    
+    const orginalChildTasks: string[] = parentTask?.[0]?.childTasks || [];
+    const newChildTasks: string[] = [];
+
+    for (let i = 0; i < orginalChildTasks.length; i++) if(orginalChildTasks[i] !== data.id) newChildTasks.push(orginalChildTasks[i])
+    
+    console.log(orginalChildTasks);
+    console.log(newChildTasks)
+
+    const { error: updateError } = await supabase
+                                    .from('parent_tasks')
+                                    .update({ childTasks: newChildTasks })
+                                    .eq('id', data.parentTask);
+
+    if (updateError) {
+    console.log(updateError.message);
+    }
+
     return NextResponse.redirect(new URL(`/tasks/${data.parentTask}`, request.url))
-}
+} 
