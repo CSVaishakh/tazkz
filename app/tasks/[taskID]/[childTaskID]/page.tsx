762e135eb900e115f13b07c412a14da88bc79f4f
Your Name <vaishakhsubramonian@gmail.com>
2025-06-23 15:29:03 +0530

    setup the api call

--- app/tasks/[taskID]/[childTaskID]/page.tsx
+++ app/tasks/[taskID]/[childTaskID]/page.tsx
@@ -1,12 +1,41 @@
+'use client';
 import { ChildTaskHeader } from "@/components/header";
-import React from "react";
+import { childTask } from "@/types/task";
+import React, { useEffect, useState, use } from "react";
 
-const ChildTask: React.FC = () => {
+interface ChildTaskPageProps{
+    params: Promise<{ parentID: string, childID: string }>;
+}
+
+const ChildTask: React.FC<ChildTaskPageProps> = ({params}) => {
+    const { parentID, childID } = use(params); 
+    const [task, setTask] = useState<childTask>({
+        id : "",
+        name : "",
+        description : "",
+        progress : "",
+        deadline : "",
+        parentTask : "",
+        notes : []
+    });
+
+    const fetchData = async () => {
+        const res = await fetch(`/api/tasks/${parentID}/${childID}`);
+        const data = await res.json();
+        setTask(data);        
+    }
+
+    useEffect(() => {
+        fetchData();
+    }[childID,parentID]);
+
     return(
         <section>
-            
             <section>
                 <ChildTaskHeader taskName=""/>
+            </section>
+            <section>
+
             </section>
             <h1>Coming Soon</h1>
         </section>
